cd4c887b8431f04f8b93a8214992f808bc73d75e
Han-Me0 <haninjamalmzher@gmail.com>
2024-12-03 15:58:33 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import Index from './pages/Index'
-import UK from './pages/UK';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import UKMap from './components/UKMap';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Index />} />
-        <Route path='/UK' element={<UK />} />
-      </Routes>
       <Navigation />
       <Routes>
         <Route path='/' element={<Index />} />
